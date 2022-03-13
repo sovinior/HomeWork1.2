8262e5e92d1c59e0f66b0930881beffb88740e8c
Ramaza <r-sh-94@mail.ru>
2022-03-13 12:34:01 +0300

    Добавил св-ва и метод

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
